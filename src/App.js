d5fdcd6b5ad0d117d3787aedc1ef52793ae457e4
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-04-01 11:49:27 -0400

    renamed navbar component to header

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Resume from "./pages/Resume";
-import Navbar from "./components/Navbar";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router basename={`${process.env.PUBLIC_URL}/`}>
-        <Navbar />
+        <Header />
           <Route exact path="/" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
