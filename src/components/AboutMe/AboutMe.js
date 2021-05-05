25ddbee9d8c27499262c6745cc60a5fa42b325d1
LostInSpace <71476446+jtoth7824@users.noreply.github.com>
2021-05-05 13:34:13 -0400

    updated bio, added skills section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,6 +3,14 @@ import aboutMePic from "../../images/pic3.jpg";
 import GitHubPic from "../../images/GitHubPic.png";
 import LinkedInPic from "../../images/LinkedInPic.png";
 import StackOverflowPic from "../../images/StackOverflowPic.png";
+import nodeIcon from "./nodeJsIcon.png";
+import reactIcon from "./reactIcon.png";
+import mongoIcon from "./mongoDbIcon.png";
+import passportIcon from "./passportIcon.png";
+import bootstrapIcon from "./bootstrapIcon.png";
+import cloudinaryIcon from "./cloudinaryIcon.png";
+import htmlCssIcon from "./javascriptIcon.png";
+import jqueryIcon from "./jQueryIcon.png";
 
 function AboutMe(props) {
     return (
@@ -17,12 +25,11 @@ function AboutMe(props) {
                     <h2 className="card-title captionSize aboutTextColor myBottomBorder">About Me</h2>
                     <img src={aboutMePic} className="image-responsive johnPic float-left padding"
                         alt="Portrait" />
-                    <p>Hi, my name is John Toth. I am a full-stack web developer currently looking for a new
-                        position. I have a B.S. in Computer Science and Engineering from the Milwaukee School of
-                        Engineering.
-                        My anticipated completion date for full-stack developer is April 2021 from George
-                        Washington University. My body of knowledge includes HTML5, CSS3, Javascript, jQuery,
-                        Ajax, JSON and Node.js.
+                    <p>Hi, my name is John Toth. I am a full-stack web developer who recently completed (April 2021) a bootcamp in full stack web development
+                        and earned a certificate from George Washington University.  I also have a B.S. in Computer Science and Engineering
+                         from the Milwaukee School of Engineering.  I am seeking an opportunity where I can apply my newly acquired skillsets and am excited to apply
+                         my knowledge of web development and testing to create better web applications.  I enjoy developing the back end of applications but also comfortable
+                         in creating the front end.  See my Skills section for a representative sample of technologies I've utilized in my projects throughout the bootcamp.
                     </p>
                     <p>Currently living in the Philadelphia area and would like to find a full-stack web
                         developer position in this area or the Trenton/Princeton area. I am open to relocating
@@ -61,6 +68,64 @@ function AboutMe(props) {
                     </div>
                     <div className="row">
                         <div className="col-xs-12">Phone number: 301-693-6688</div>
+                    </div>
+                </div>
+            </div>
+            <div className="card">
+                <div className="card-body">
+                    <h2 className="card-title captionSize aboutTextColor myBottomBorder">Skills</h2>
+                    <div className="row">
+                        <div className="col justify-content-center">
+                            <a href="https://nodejs.org/en/" className="padding"
+                                target="_blank"  rel="noreferrer noopener"><img className="d-block m-auto" src={nodeIcon}
+                                style={{height:"50px", width:"80px"}} alt="node.js"/></a>
+                        </div>
+                        <div className="col justify-content-center">
+                            <a href="https://reactjs.org/" className="padding" target="_blank" rel="noreferrer noopener"><img
+                                    className="d-block p-auto m-auto" src={reactIcon} style={props.style}
+                                     alt="React" /></a>
+                        </div>
+                        <div className="col justify-content-center">
+                            <a href="https://www.mongodb.com/" className="padding"
+                                target="_blank" rel="noreferrer noopener"><img className="d-block m-auto"
+                                    src={mongoIcon} style={{height:"50px", width:"80px"}}
+                                    alt="mongoDb" /></a>
+                        </div>
+                    </div>
+                    <div className="row">
+                    <div className="col justify-content-center">
+                            <a href="http://www.passportjs.org/" className="padding"
+                                target="_blank" rel="noreferrer noopener"><img className="d-block m-auto"
+                                    src={passportIcon} style={props.style}
+                                    alt="passport" /></a>
+                        </div>
+                        <div className="col justify-content-center">
+                            <a href="https://getbootstrap.com/" className="padding"
+                                target="_blank" rel="noreferrer noopener"><img className="d-block m-auto"
+                                    src={bootstrapIcon} style={props.style}
+                                    alt="bootstrap" /></a>
+                        </div>
+                        <div className="col justify-content-center">
+                            <a href="https://cloudinary.com/" className="padding"
+                                target="_blank" rel="noreferrer noopener"><img className="d-block m-auto"
+                                    src={cloudinaryIcon} style={{height:"60px", width:"100px"}}
+                                    alt="cloudinary" /></a>
+                        </div>
+                    </div>
+                    
+                    <div className="row">
+                    <div className="col justify-content-center">
+                            <a href="https://www.javascript.com/" className="padding"
+                                target="_blank" rel="noreferrer noopener"><img className="d-block m-auto"
+                                    src={htmlCssIcon} style={{height:"75px", width:"100px"}}
+                                    alt="html css javascript" /></a>
+                        </div>
+                        <div className="col justify-content-center">
+                            <a href="https://jquery.com/" className="padding"
+                                target="_blank" rel="noreferrer noopener"><img className="d-block m-auto"
+                                    src={jqueryIcon} style={{height:"60px", width:"100px"}}
+                                    alt="jquery" /></a>
+                        </div>
                     </div>
                 </div>
             </div>
